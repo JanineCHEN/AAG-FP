2c4bbaa7695fb9a86a3cdd72354de9a45a092832
JanineCHEN <37527041+JanineCHEN@users.noreply.github.com>
2021-11-05 12:16:04 +0800

    Add files via upload

---
+++ Text_Segmentation/loss.py
@@ -0,0 +1,307 @@
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+
+from models.BaseModels import BaseModule
+
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+Tensor = FloatTensor
+
+
+class MultiClassFocalLoss(nn.Module):
+    # focal loss
+    # https://arxiv.org/pdf/1708.02002.pdf
+    # copy from  https://github.com/clcarwin/focal_loss_pytorch
+
+    # alpha=0.75 gives the best for this project
+    def __init__(self, gamma=2.0, alpha=0.25, size_average=True):
+        super(MultiClassFocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1 - alpha])
+
+        elif isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+
+        logpt = logpt.view(-1)
+        pt = logpt.data.exp()
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * at
+
+        loss = -1 * (1 - pt) ** self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+
+class BinaryFocalLoss(nn.BCEWithLogitsLoss):
+    # gamma 0 gives the best AP scores
+    def __init__(self, gamma=0, background_weights=1, words_weights=2):
+        super(BinaryFocalLoss, self).__init__(size_average=True, reduce=False)
+        self.gamma = gamma
+        self.background_weights = background_weights
+        self.words_weights = words_weights
+
+    def forward(self, input, target):
+        input = self.flatten_images(input)
+        target = self.flatten_images(target)
+        weights = torch.where(target > 0, torch.ones_like(target) * self.words_weights,  # words are 1
+                              torch.ones_like(target) * self.background_weights)
+        pt = F.logsigmoid(-input * (target * 2 - 1))
+        loss = F.binary_cross_entropy_with_logits(input, target, weight=weights, size_average=True, reduce=False)
+
+        loss = (pt * self.gamma).exp() * loss
+        return loss.mean()
+
+    @staticmethod
+    def flatten_images(x):
+        assert x.dim() == 4 and x.size(1) == 1
+        x = x.view(x.size(0), x.size(1), -1)  # N,C,H,W => N,C,H*W
+        x = x.transpose(1, 2)  # N,C,H*W => N,H*W,C
+        x = x.contiguous().view(-1, x.size(2))
+        return x
+
+
+class SoftBootstrapCrossEntropy(nn.BCELoss):
+    """
+    TRAINING DEEP NEURAL NETWORKS ON NOISY LABELS WITH BOOTSTRAPPING (https://arxiv.org/pdf/1412.6596.pdf)
+    # Tensorflow: https://github.com/tensorflow/models/blob/f87a58cd96d45de73c9a8330a06b2ab56749a7fa/research/object_detection/core/losses.py#L275-L336
+    ++++   Use with caution ++++
+    with this loss, the model may learn to detect words that are not labeled.
+    but  not all words are necessary whited out
+    """
+
+    def __init__(self, beta=0.95, background_weight=1, words_weight=2,
+                 size_average=True, reduce=True):
+        super(SoftBootstrapCrossEntropy, self).__init__(size_average=size_average, reduce=reduce)
+        self.beta = beta
+        self.background_weight = background_weight
+        self.words_weight = words_weight
+        self.size_average = size_average
+        self.reduce = reduce
+
+    def forward(self, input, target):
+        # if a pixel's probability > 0.5, then assume it is true since labels might be noisy
+        input = self.flatten_images(input)
+        target = self.flatten_images(target)
+        weights = torch.where(target > 0, torch.ones_like(target) * self.words_weight,  # words are 1
+                              torch.ones_like(target) * self.background_weight)
+
+        bootstrap_target = self.beta * target + (1 - self.beta) * (F.sigmoid(input) > 0.5).float()
+        return F.binary_cross_entropy_with_logits(input, bootstrap_target, weight=weights,
+                                                  size_average=self.size_average, reduce=self.reduce)
+
+    @staticmethod
+    def flatten_images(x):
+        assert x.dim() == 4 and x.size(1) == 1
+        x = x.view(x.size(0), x.size(1), -1)  # N,C,H,W => N,C,H*W
+        x = x.transpose(1, 2)  # N,C,H*W => N,H*W,C
+        x = x.contiguous().view(-1, x.size(2))
+        return x
+
+
+# +++++++++++++++++++++++++++++++++++++
+#           Loss for classification with LSTM
+# -------------------------------------
+
+# reference: Multi-label Image Recognition by Recurrently Discovering Attentional Regions
+# by Wang, chen,  Li, Xu, and Lin
+
+class BCERegionLoss(nn.Module):
+    def __init__(self):
+        super(BCERegionLoss, self).__init__()
+        self.anchor_box = FloatTensor([(0.4, 0.4), (0.4, -0.4), (-0.4, -0.4), (-0.4, 0.4)]).unsqueeze(-1)
+        self.scale_alpha = FloatTensor([1])
+        self.positive_beta = FloatTensor([0.2])
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def scale_loss(self, scale):
+        #         assert scale.size(1) == scale.size(2)
+        sx = scale[:, 0, 0]
+        ls = torch.pow(F.relu(torch.abs(sx) - self.scale_alpha), 2)
+        sy = scale[:, 1, 1]
+        ly = torch.pow(F.relu(torch.abs(sy) - self.scale_alpha), 2)
+        positive_loss = F.relu(self.positive_beta - sx) + F.relu(self.positive_beta - sy)
+
+        loss = 0.1 * positive_loss + ls + ly
+        return loss.sum().view(1)
+
+    def anchor_loss(self, attention_region):
+        # input: num_class, 2 (anchor x, y) , 1   -self.anchor_box
+        distance = 0.5 * torch.pow(attention_region - self.anchor_box, 2).sum(1)
+        return distance.sum().view(1)
+
+    def forward(self, input, target):
+        category, transform_box = input
+        # select the highest probabilities on number of labels among iterations
+        scores, index = category.max(1)
+        bce_loss = self.bce(scores, target)
+        # all regions' predictions are checked
+        # bce_loss = sum([self.bce(category[:, i, :], target) for i in category.size(1)])
+        # bce_loss = FloatTensor([0])
+        # for i in range(category.size(1)):
+        #     bce_loss = bce_loss + self.bce(category[:, i, :], target)
+        # bce_loss = bce_loss / category.size(1)
+
+        regions = transform_box[:, :, :, 2:]
+        region_loss = torch.cat([self.anchor_loss(i) for i in regions]).mean()
+
+        scales = transform_box[:, :, :, :2]
+        scale_loss = torch.cat([self.scale_loss(i) for i in scales]).mean()
+
+        # spatial transform theta matrix (batch, 2, 3)
+        # sum over the second axis so that transformed regions will not be 0 padded
+        boundary = torch.abs(transform_box).sum(-1)
+        boundary = torch.pow(F.relu(boundary - 1), 2)
+        boundary_loss = boundary.view(boundary.size(0), -1).sum(-1).mean()
+        return bce_loss, bce_loss + 0.2 * region_loss + 0.05 * scale_loss + 0.1 * boundary_loss
+
+
+# +++++++++++++++++++++++++++++++++++++
+#           Loss for inpainting
+# -------------------------------------
+
+class InpaintingLoss(nn.Module):
+    # https://github.com/naoto0804/pytorch-inpainting-with-partial-conv/blob/master/loss.py
+    # Image Inpainting for Irregular Holes Using Partial Convolutions
+    def __init__(self, feature_encoder, feature_range=3):
+        super(InpaintingLoss, self).__init__()
+        self.l1 = nn.L1Loss()
+        self.feature_encoder = FeatureExtractor(feature_encoder, feature_range)
+        # self.normalize = Normalize(mean=[0.485, 0.456, 0.406],
+        #                            std=[0.229, 0.224, 0.225])
+
+    def forward(self, raw_input, mask, output, origin):
+        comp_img = mask * raw_input + (1 - mask) * output
+
+        # per-pixel loss
+        loss_validate = self.l1(mask * output, mask * origin)
+        loss_hole = self.l1((1 - mask) * output, (1 - mask) * origin)
+
+        # total variation (smoothing penalty)
+        loss_total_var = total_variation_loss(comp_img)
+
+        # extra features
+        feature_comp = self.feature_encoder(comp_img)
+        feature_output = self.feature_encoder(output)
+        feature_origin = self.feature_encoder(origin)
+
+        # perceptual loss
+        loss_perceptual_1 = sum(map(lambda x, y: self.l1(x, y), feature_comp, feature_origin))
+        loss_perceptual_2 = sum(map(lambda x, y: self.l1(x, y), feature_output, feature_origin))
+        loss_perceptual = loss_perceptual_1 + loss_perceptual_2
+
+        # style loss
+        loss_style_1 = sum(map(lambda x, y: self.l1(gram_matrix(x), gram_matrix(y)),
+                               feature_output, feature_origin))
+        loss_style_2 = sum(map(lambda x, y: self.l1(gram_matrix(x), gram_matrix(y)),
+                               feature_comp, feature_origin))
+        loss_style = loss_style_1 + loss_style_2
+
+        # weights are recommended in the paper P7
+        loss = 1.0 * loss_validate + 6.0 * loss_hole + \
+               0.1 * loss_total_var + 0.05 * loss_perceptual + 120 * loss_style
+        return loss
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, encoder, feature_range=3):
+        super(FeatureExtractor, self).__init__()
+        self.layers = nn.Sequential(*[encoder.features[i] for i in range(feature_range)])
+        for layer in self.layers:
+            for param in layer.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        out = []
+        for layer in self.layers:
+            x = layer(x)
+            out.append(x)
+        return out
+
+
+class VggExtractor(BaseModule):
+    def __init__(self, pretrained=True):
+        super(VggExtractor, self).__init__()
+        vgg = torchvision.models.vgg16(pretrained=pretrained)
+        feature1 = nn.Sequential(*vgg.features[:5])
+        feature2 = nn.Sequential(*vgg.features[5:10])
+        feature3 = nn.Sequential(*vgg.features[10:17])
+        self.features = nn.Sequential(*[feature1, feature2, feature3])
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+    def forward(self, img):
+        result = []
+        for layer in self.features.children():
+            img = layer(img)
+            result.append(img)
+        return result
+
+
+class ResNetExtractor(BaseModule):
+    def __init__(self, pretrained=True):
+        super(ResNetExtractor, self).__init__()
+        resnet = torchvision.models.resnet50(pretrained=pretrained)
+        feature1 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1)
+        feature2 = nn.Sequential(resnet.layer2)
+        feature3 = nn.Sequential(resnet.layer3)
+        self.features = nn.Sequential(*[feature1, feature2, feature3])
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+    def forward(self, *x):
+        raise NotImplemented
+
+
+class XecptionExtractor(BaseModule):
+    def __init__(self, pretrain_model, pretrain_checkpoint):
+        super(XecptionExtractor, self).__init__()
+        pretrain_model.load_state_dict(torch.load(pretrain_checkpoint))
+        xecption = pretrain_model.encoder
+        feature1 = xecption.entry_flow_1
+        feature2 = xecption.entry_flow_2
+        feature3 = xecption.middle_flow
+        self.features = nn.Sequential(*[feature1, feature2, feature3])
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+    def forward(self, *x):
+        raise NotImplemented
+
+
+def gram_matrix(feat):
+    # https://github.com/pytorch/examples/blob/master/fast_neural_style/neural_style/utils.py
+    (b, ch, h, w) = feat.size()
+    feat = feat.view(b, ch, h * w)
+    feat_t = feat.transpose(1, 2)
+    gram = torch.bmm(feat, feat_t) / (ch * h * w)
+    return gram
+
+
+def total_variation_loss(image):
+    # shift one pixel and get difference (for both x and y direction)
+    loss = torch.mean(torch.abs(image[:, :, :, :-1] - image[:, :, :, 1:])) + \
+           torch.mean(torch.abs(image[:, :, :-1, :] - image[:, :, 1:, :]))
+    return loss
---
+++ Text_Segmentation/models/common.py
@@ -0,0 +1,218 @@
+import torch
+from torch import nn
+from torch.nn.functional import affine_grid, grid_sample
+
+from .BaseModels import BaseModule, Conv_block
+
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+Tensor = FloatTensor
+
+
+class SpatialChannelSqueezeExcitation(BaseModule):
+    # https://arxiv.org/abs/1709.01507
+    # https://arxiv.org/pdf/1803.02579v1.pdf
+    def __init__(self, in_channel, reduction=16, activation=nn.ReLU()):
+        super(SpatialChannelSqueezeExcitation, self).__init__()
+        linear_nodes = max(in_channel // reduction, 4)  # avoid only 1 node case
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.channel_excite = nn.Sequential(
+            # check the paper for the number 16 in reduction. It is selected by experiment.
+            nn.Linear(in_channel, linear_nodes),
+            activation,
+            nn.Linear(linear_nodes, in_channel),
+            nn.Sigmoid()
+        )
+        self.spatial_excite = nn.Sequential(
+            nn.Conv2d(in_channel, 1, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        # channel
+        channel = self.avg_pool(x).view(b, c)
+        # channel = F.avg_pool2d(x, kernel_size=(h,w)).view(b,c)
+        cSE = self.channel_excite(channel).view(b, c, 1, 1)
+        x_cSE = torch.mul(x, cSE)
+
+        # spatial
+        sSE = self.spatial_excite(x)
+        x_sSE = torch.mul(x, sSE)
+        return torch.add(x_cSE, x_sSE)
+
+
+def add_SCSE_block(model_block, in_channel=None):
+    if in_channel is None:
+        # the first layer is assumed to be conv
+        in_channel = model_block[0].out_channels
+    model_block.add_module("SCSE", SpatialChannelSqueezeExcitation(in_channel))
+
+
+class ASP(BaseModule):
+    # Atrous Spatial Pyramid Pooling with Image Pooling
+    # add Vortex pooling https://arxiv.org/pdf/1804.06242v1.pdf
+    def __init__(self, in_channel=256, out_channel=256, act_fn=None, asp_rate=(3, 9, 27)):
+        super(ASP, self).__init__()
+
+        self.asp = nn.Sequential(
+            nn.Sequential(*Conv_block(in_channel, out_channel, kernel_size=3, stride=1, padding=1,
+                                      bias=False, BN=True, activation=act_fn)),
+            nn.Sequential(nn.AvgPool2d(kernel_size=asp_rate[0], stride=1, padding=(asp_rate[0] - 1) // 2),
+                          *Conv_block(in_channel, out_channel, kernel_size=3, stride=1, padding=asp_rate[0],
+                                      dilation=asp_rate[0], bias=False, BN=True, activation=act_fn)),
+            nn.Sequential(nn.AvgPool2d(kernel_size=asp_rate[1], stride=1, padding=(asp_rate[1] - 1) // 2),
+                          *Conv_block(in_channel, out_channel, kernel_size=3, stride=1, padding=asp_rate[1],
+                                      dilation=asp_rate[1], bias=False, BN=True, activation=act_fn)),
+            nn.Sequential(nn.AvgPool2d(kernel_size=asp_rate[2], stride=1, padding=(asp_rate[2] - 1) // 2),
+                          *Conv_block(in_channel, out_channel, kernel_size=3, stride=1, padding=asp_rate[2],
+                                      dilation=asp_rate[2], bias=False, BN=True, activation=act_fn))
+        )
+
+        # May be not necessary since only words are detected
+        # """ To see why adding global average, please refer to 3.1 Global Context in
+        #  https://www.cs.unc.edu/~wliu/papers/parsenet.pdf """
+
+        # self.img_pooling = nn.Sequential(
+        #     nn.AdaptiveAvgPool2d(1),
+        #     *Conv_block(in_channel, out_channel, kernel_size=1, bias=False, BN=True, activation=act_fn))
+
+        self.out_conv = nn.Sequential(*Conv_block(out_channel * 4, out_channel, kernel_size=1, bias=False,
+                                                  BN=True, activation=act_fn))
+        # self.initialize_weights()
+        # self.selu_init_params()
+
+    def forward(self, x):
+        # avg_pool = self.img_pooling(x)
+        # avg_pool = [F.interpolate(avg_pool, size=x.shape[2:], mode='bilinear', align_corners=False)]
+        asp_pool = [layer(x) for layer in self.asp.children()]
+        # out_features = torch.cat(avg_pool + asp_pool, dim=1)
+        asp_pool = torch.cat(asp_pool, dim=1)
+        out = self.out_conv(asp_pool)
+        return out
+
+
+class RFB(BaseModule):
+    # receptive fiedl block  https://arxiv.org/abs/1711.07767 with some changes
+    # reference: https://github.com/ansleliu/LightNet/blob/master/modules/rfblock.py
+    # https://github.com/ruinmessi/RFBNet/blob/master/models/RFB_Net_mobile.py
+    def __init__(self, in_channel, out_channel, activation, add_sece=False):
+        super(RFB, self).__init__()
+        asp_rate = [5, 17, 29]
+        self.act_fn = activation
+        # self.act_fn = activation
+        self.input_down_channel = nn.Sequential(
+            *Conv_block(in_channel, out_channel, kernel_size=1, bias=True, BN=True, activation=activation))
+
+        rfb_linear_conv = [nn.Conv2d(out_channel * 4, out_channel, kernel_size=1, bias=True)]
+        if add_sece:
+            rfb_linear_conv.append(SpatialChannelSqueezeExcitation(in_channel=out_channel, activation=activation))
+        self.rfb_linear_conv = nn.Sequential(*rfb_linear_conv)
+
+        self.rfb = nn.Sequential(
+            self.make_pooling_branch(in_channel, out_channel, out_channel, conv_kernel=1,
+                                     astro_rate=1, activation=activation, half_conv=False),
+            self.make_pooling_branch(in_channel, out_channel // 2, out_channel, conv_kernel=3,
+                                     astro_rate=asp_rate[0], activation=activation, half_conv=True),
+            self.make_pooling_branch(in_channel, out_channel // 2, out_channel, conv_kernel=5,
+                                     astro_rate=asp_rate[1], activation=activation, half_conv=True),
+            self.make_pooling_branch(in_channel, out_channel // 2, out_channel, conv_kernel=7,
+                                     astro_rate=asp_rate[2], activation=activation, half_conv=True)
+        )
+
+    @staticmethod
+    def make_pooling_branch(in_channel, mid_channel, out_channel, conv_kernel, astro_rate, activation, half_conv=False):
+        # from the paper: we use a 1 x n plus an nx1 conv-layer to take place of the original nxn convlayer
+        # similar to EffNet style
+        if half_conv:
+            m = nn.Sequential(
+                *Conv_block(in_channel, mid_channel, kernel_size=1, padding=0,
+                            bias=False, BN=True, activation=activation),
+                *Conv_block(mid_channel, 3 * mid_channel // 2, kernel_size=(1, conv_kernel),
+                            padding=(0, (conv_kernel - 1) // 2), bias=False, BN=True, activation=None),
+                *Conv_block(3 * mid_channel // 2, out_channel, kernel_size=(conv_kernel, 1),
+                            padding=((conv_kernel - 1) // 2, 0), bias=False, BN=True, activation=None),
+                *Conv_block(out_channel, out_channel, kernel_size=3, dilation=astro_rate, padding=astro_rate,
+                            bias=False, BN=True, activation=activation, groups=out_channel))
+        else:
+            m = nn.Sequential(
+                *Conv_block(in_channel, out_channel, kernel_size=conv_kernel, padding=(conv_kernel - 1) // 2,
+                            bias=False, BN=True, activation=activation),
+                *Conv_block(out_channel, out_channel, kernel_size=3, dilation=astro_rate, padding=astro_rate,
+                            bias=False, BN=True, activation=activation, groups=out_channel)
+            )
+
+        return m
+
+    def forward(self, x):
+        # feature pooling
+        rfb_pool = [layer(x) for layer in self.rfb.children()]
+        rfb_pool = torch.cat(rfb_pool, dim=1)
+        rfb_pool = self.rfb_linear_conv(rfb_pool)
+
+        # skip connection
+        resi = self.input_down_channel(x)
+        return self.act_fn(rfb_pool + resi)
+
+
+class CNNLSTMClassifier(BaseModule):
+    def __init__(self, num_class, lstm_in_channel, lstm_hidden=256, act_fn=nn.LeakyReLU(0.3)):
+        super(CNNLSTMClassifier, self).__init__()
+        self.global_avg = nn.AdaptiveAvgPool2d(1)
+        self.lstm = nn.LSTM(lstm_in_channel, lstm_hidden, num_layers=1, batch_first=True)
+        self.lstm_linear_z = nn.Sequential(nn.Linear(lstm_hidden, lstm_hidden), act_fn)
+        self.lstm_linear_score = nn.Linear(lstm_hidden, num_class)
+        self.st_theta_linear = nn.Linear(lstm_hidden, 2 * 3)
+        self.anchor_box = FloatTensor([(0.4, 0.4), (0.4, -0.4), (-0.4, -0.4), (-0.4, 0.4)
+                                       ])
+
+    @staticmethod
+    def spatial_transformer(input_image, theta):
+        # reference: Spatial Transformer Networks https://arxiv.org/abs/1506.02025
+        # https://blog.csdn.net/qq_39422642/article/details/78870629
+        grids = affine_grid(theta, input_image.size())
+        output_img = grid_sample(input_image, grids)
+        return output_img
+
+    def forward(self, input_img):
+        # Multi-label Image Recognition by Recurrently Discovering Attentional Regions by Wang, chen,  Li, Xu, and Lin
+        # LSTM input: step size is one, feature size is num_class (channels)
+
+        img = input_img
+        batch = input_img.size(0)
+
+        category_scores = []
+        transform_box = []
+        # h = c = torch.zeros(1, batch, self.lstm.hidden_size).cuda()
+        # features = self.global_avg(img).view(batch, 1, -1)
+        features = img.view(batch, 1, -1)
+        y, (h, c) = self.lstm(features)
+        s = self.lstm_linear_score(y.view(batch, -1))
+        category_scores.append(s)
+
+        for i in range(4):  # 4 anchor points so repeated 4 times
+            z = self.lstm_linear_z(h.transpose(0, 1).view(batch, -1))  # y.view(batch, -1)
+            st_theta = self.st_theta_linear(z).view(batch, 2, 3)
+            st_theta[:, :, -1] = st_theta[:, :, -1] + self.anchor_box[i]
+
+            st_theta[:, 1, 0] = 0 * st_theta[:, 1, 0]
+            st_theta[:, 0, 1] = 0 * st_theta[:, 0, 1]
+
+            transform_box.append(st_theta)
+
+            img = self.spatial_transformer(input_img, st_theta)
+            features = img.view(batch, 1, -1)
+            # features = self.global_avg(img).view(batch, 1, -1)
+
+            # y.size = batch, seq_len (1) , num_direc*hidden_size
+            # h, c size = num_layer*bi-direc, batch, hidden_size
+            y, (h, c) = self.lstm(features, (h, c))
+
+            s = self.lstm_linear_score(y.view(batch, -1))
+            # the paper use the hidden state to get scores  h.transpose(0, 1).view(batch, -1)
+            category_scores.append(s)
+
+        category_scores = torch.stack(category_scores, dim=1)  # size: batch, category regions, category
+        transform_box = torch.stack(transform_box, dim=1)  # the first one is free. size: batch, regions, 2,3
+        return category_scores, transform_box
